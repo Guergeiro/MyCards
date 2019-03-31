fe6ff69d11ffeea8bec526b40ea5dc2cdee0aca3
Guergeiro <breno-salles@hotmail.com>
2019-03-31 20:27:08 +0100

    Proof of concept. BS only

--- templates/js/header.js
+++ templates/js/header.js
@@ -1,13 +1,16 @@
 document.querySelector("nav").querySelector(".navbar-toggler").addEventListener("click", function () {
 	let sidebar = document.querySelector("#ourNavbar");
-	if (sidebar.style.width == "300px") {
+	if (sidebar.style.width == "250px") {
+		document.querySelector("nav").querySelector(".navbar-toggler").querySelector("i").classList.remove("fa-times");
+		document.querySelector("nav").querySelector(".navbar-toggler").querySelector("i").classList.add("fa-bars");
 		sidebar.style.width = "0";
-		sidebar.style.marginLeft = "-300px";
+		sidebar.style.marginLeft = "-250px";
 	} else {
-		this.style.marginLeft = "0px";
-		sidebar.style.width = "300px";
+		document.querySelector("nav").querySelector(".navbar-toggler").querySelector("i").classList.remove("fa-bars");
+		document.querySelector("nav").querySelector(".navbar-toggler").querySelector("i").classList.add("fa-times");
+		sidebar.style.width = "250px";
 		sidebar.style.marginLeft = "0";
 	}
 });
 
-document.querySelector("#ourNavbar").style.marginTop = document.querySelector("nav").offsetHeight;
+document.querySelector("#ourNavbar").style.paddingTop = document.querySelector("nav").offsetHeight;
