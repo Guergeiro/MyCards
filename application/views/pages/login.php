b5bb633a5fdeb1752e107e385b712f9f58b903fb
EdwinOliveira <39379560+EdwinOliveira@users.noreply.github.com>
2019-04-17 15:27:53 +0100

    update-projecto

---
+++ application/views/pages/login.php
@@ -0,0 +1,75 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="bgIMG fixed-top"></div>
+            <div class="row justify-content-center">
+
+                <div class="col-md-4 mt-3">
+                    <!-- Default form login -->
+                    <div id="logform" class="card">
+                        <div class="card-header bg-info text-center">
+                            <p class="h4 text-white mt-3">Faça Login</p>
+                        </div>
+                        <form class="pl-5 pr-5 pb-5">
+
+                            <!-- Material outline input with prefix-->
+                            <div class="md-form md-outline">
+                                <i class="fas fa-envelope prefix"></i>
+                                <input type="text" id="inputIconEx1" class="form-control">
+                                <label for="inputIconEx1">E-mail</label>
+                                <small id="emailHelp2" class="form-text text-muted">O seu Email nunca irá ser partilhado com ninguém.</small>
+                            </div>
+
+                            <!-- Material outline input with prefix-->
+                            <div class="md-form md-outline">
+                                <i class="fas fa-lock prefix"></i>
+                                <input type="password" id="inputValidationEx2" class="form-control ">
+                                <label for="inputIconEx2">Password</label>
+                            </div>
+
+                            <div id="optionsLogin" class="d-flex justify-content-around ">
+                                <div>
+                                    <!-- Remember me -->
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+                                        <label class="custom-control-label" for="defaultLoginFormRemember">Relembrar-me</label>
+                                    </div>
+                                </div>
+                                <div id="forgot-password">
+                                    <!-- Forgot password -->
+                                    <a href="">Esqueceu-se da password?</a>
+                                </div>
+                            </div>
+
+                            <!-- Sign in button -->
+                            <button class="btn text-info btn-block my-4 border border-info rounded-pill rounded-lg" type="submit">Entrar</button>
+
+                            <div class="content text-center">
+                                <!-- Register -->
+                                <p>Não é membro?
+                                    <a href="">Registe-se</a>
+                                </p>
+
+                                <!-- Social login -->
+                                <p>ou registe-se com:</p>
+
+                                <a class="light-blue-text mx-2">
+                                    <i class="fab fa-facebook-f fa-lg"></i>
+                                </a>
+                                <a class="light-blue-text mx-2">
+                                    <i class="fab fa-twitter fa-lg "></i>
+                                </a>
+                                <a class="light-blue-text mx-2 ">
+                                    <i class="fab fa-linkedin-in fa-lg"></i>
+                                </a>
+                            </div>
+
+                        </form>
+                        <!-- Default form login -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
