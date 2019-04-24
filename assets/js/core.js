a02179cca190e1a1dfecdd4b0ff8588fbe67b4a1
diogofalken <diogofalken0@gmail.com>
2019-04-24 18:31:15 +0100

    Working on navbar improvements.

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,7 +1,15 @@
 /*main.js, containes in itself the main scripts that allow the components that are similar in every page, in this case the Navbar and SideBar, to perform certain actions*/
 
 function showSideMenu() {
-    document.getElementById("sidebarMenu").classList.toggle('active');
+	document.getElementById("sidebarMenu").classList.toggle("active");
 }
 
-
+$(".fa-bars").click(function(e) {
+	console.log($(".sidenav").width());
+	if ($(".sidenav").width() == 318.75) {
+		$(".sidenav").css("width", "0px");
+	}
+	if ($(".sidenav").width() == -0.25) {
+		$(".sidenav").css("width", "320px");
+	}
+});
