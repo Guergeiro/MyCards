ea6a9cffee6ccd658fc5cf3a53de512d45978976
Guergeiro <breno-salles@hotmail.com>
2019-05-03 22:56:30 +0100

    Added MDBootstrap

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,6 @@
 <html>
 
 <head>
-	<!-- MetaDados -->
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,14 +9,14 @@
 	<!-- Titulo -->
 	<title><?php echo ucfirst($page); ?></title>
 
-	<!-- Boostrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
 
-	<!-- FontAwesome -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<!-- Bootstrap core CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.0/css/mdb.min.css" rel="stylesheet">
 
 	<!-- Core CSS -->
 	<link href="<?php echo base_url("assets/css/core.css"); ?>" rel="stylesheet">
@@ -37,30 +36,30 @@
 		<div class="collapse navbar-collapse text-center justify-content-end" id="navbar">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link" href="<?php echo base_url("notificacoes"); ?>">
+					<a class="nav-link py-0" href="<?php echo base_url("notificacoes"); ?>">
 						<i class="fas fa-bell fa-fw fa-lg"></i>
 						<span class="badge badge-danger ml-n3 align-top rounded-circle">4</span>
 						<span class="d-md-none">Notificações</span>
 					</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="<?php echo base_url("mensagens"); ?>">
+					<a class="nav-link py-0" href="<?php echo base_url("mensagens"); ?>">
 						<i class="fas fa-envelope fa-fw fa-lg"></i>
 						<span class="badge badge-danger ml-n3 align-top rounded-circle">4</span>
 						<span class="d-md-none">Mensangens</span>
 					</a>
 				</li>
 				<li class="nav-item d-inline-block dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+					<a class="nav-link dropdown-toggle py-0" href="#" id="navbarDropdown" role="button"
 						data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<img src="<?php echo base_url('assets/avatar/920983_516047081776703_299333251_o.jpg'); ?>" alt="avatar_img" srcset="" width="24" height="24" class="rounded-circle">
 						<span>John Doe</span>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 						<a class="dropdown-item" href="<?php echo base_url("perfil"); ?>"><i class="fas fa-user fa-fw"></i> Perfil</a>
-						<a class="dropdown-item" href="#"><i class="fas fa-cog fa-fw"></i> Definições</a>
+						<a class="dropdown-item" href="<?php echo base_url("definicoes"); ?>"><i class="fas fa-cog fa-fw"></i> Definições</a>
 						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="#"><i class="fas fa-power-off fa-fw"></i> Logout</a>
+						<a class="dropdown-item" href="<?php echo base_url("logout"); ?>"><i class="fas fa-power-off fa-fw"></i> Logout</a>
 					</div>
 				</li>
 			</ul>
@@ -70,7 +69,7 @@
 	<div class="h-100 position-fixed text-dark bg-light" id="ourNavbar" style="margin-left: -320px;">
 		<ul class="list-group">
 			<a class="list-group-item bg-transparent rounded-0 border-0 p-3 text-dark text-decoration-none active"
-				href="#">
+				href="<?php echo base_url("dashboard"); ?>">
 				<i class="fas fa-chart-area fa-fw"></i> Dashboard
 			</a>
 			<a class="list-group-item bg-transparent rounded-0 border-0 p-3" role="button"
@@ -79,10 +78,10 @@
 				<i class="fas fa-chevron-down float-right fa-fw"></i>
 			</a>
 			<div class="collapse bg-grey" id="empresa">
-				<a class="d-block text-dark text-decoration-none p-3" href="#">
+				<a class="d-block text-dark text-decoration-none p-3" href="<?php echo base_url("visualizarEmpresa"); ?>">
 					Visualizar empresa
 				</a>
-				<a class="d-block text-dark text-decoration-none p-3" href="#">
+				<a class="d-block text-dark text-decoration-none p-3" href="<?php echo base_url("definicoesEmpresa"); ?>">
 					Definições empresa
 				</a>
 			</div>
@@ -92,15 +91,15 @@
 				<i class="fas fa-chevron-down float-right fa-fw"></i>
 			</a>
 			<div class="collapse bg-grey" id="colaboradores">
-				<a class="d-block text-dark text-decoration-none p-3" href="#">
+				<a class="d-block text-dark text-decoration-none p-3" href="<?php echo base_url("visualizarColaboradores"); ?>">
 					Visualizar colaboradores
 				</a>
-				<a class="d-block text-dark text-decoration-none p-3" href="#">
+				<a class="d-block text-dark text-decoration-none p-3" href="<?php echo base_url("definicoesColaboradores"); ?>">
 					Definições colaboradores
 				</a>
 			</div>
 			<a class="list-group-item bg-transparent rounded-0 border-0 p-3 text-dark text-decoration-none"
-				href="#">
+				href="<?php echo base_url("clientes"); ?>">
 				<i class="fas fa-users fa-fw"></i> Clientes
 			</a>
 			<a class="list-group-item bg-transparent rounded-0 border-0 p-3" role="button"
@@ -109,13 +108,13 @@
 				<i class="fas fa-chevron-down float-right fa-fw"></i>
 			</a>
 			<div class="collapse bg-grey" id="campanhas">
-				<a class="d-block text-dark text-decoration-none p-3" href="#">
+				<a class="d-block text-dark text-decoration-none p-3" href="<?php echo base_url("criarCampanha"); ?>">
 					Criar campanha
 				</a>
-				<a class="d-block text-dark text-decoration-none p-3" href="#">
+				<a class="d-block text-dark text-decoration-none p-3" href="<?php echo base_url("listarCampanha"); ?>">
 					Listar campanha
 				</a>
-				<a class="d-block text-dark text-decoration-none p-3" href="#">
+				<a class="d-block text-dark text-decoration-none p-3" href="<?php echo base_url("ativarCampanha"); ?>">
 					Ativar campanha
 				</a>
 			</div>
--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -1,31 +1,30 @@
+let inputs = document.querySelectorAll("input[type=password]");
+inputs.forEach((input) => {
+	input.addEventListener("input", function () {
+		if (input.value.trim().length == 0) {
+			input.classList.remove("is-valid", "is-invalid");
+			input.nextElementSibling.classList.remove("invalid-tooltip", "valid-tooltip");
+			input.nextElementSibling.innerHTML = "";
+		} else {
+			let regex = new RegExp("[A-Z]");
+			if (regex.test(input.value.trim())) {
+				input.classList.add("is-valid");
+				input.classList.remove("is-invalid");
+				input.nextElementSibling.classList.remove("invalid-tooltip");
+				input.nextElementSibling.classList.add("valid-tooltip");
+				input.nextElementSibling.innerHTML = "Correto!";
+			} else {
+				input.classList.remove("is-valid");
+				input.classList.add("is-invalid");
+				input.nextElementSibling.classList.add("invalid-tooltip");
+				input.nextElementSibling.classList.remove("valid-tooltip");
+				input.nextElementSibling.innerHTML = "A password deve conter no mínimo 8 caracteres, e 1 número.";
+			}
+		}
+	});
+});
+
 function validation(form) {
-	let inputs = form.querySelectorAll("input[type=password]");
-	let regex = new RegExp("[A-Z]");
-	if (inputs[0].value.trim().length == 0) {
-		inputs[0].classList.remove("is-valid", "is-invalid");
-		inputs[1].classList.remove("is-valid", "is-invalid");
-
-		inputs[0].nextElementSibling.classList.remove("invalid-tooltip", "valid-tooltip");
-		inputs[0].nextElementSibling.innerHTML = "";
-		inputs[1].nextElementSibling.classList.remove("invalid-tooltip", "valid-tooltip");
-		inputs[1].nextElementSibling.innerHTML = "";
-		return false;
-	}
-	if (regex.test(inputs[0].value)) {
-		inputs[0].classList.add("is-valid");
-		inputs[0].classList.remove("is-invalid");
-		inputs[0].nextElementSibling.classList.remove("invalid-tooltip");
-		inputs[0].nextElementSibling.classList.add("valid-tooltip");
-		inputs[0].nextElementSibling.innerHTML = "Correto!";
-	} else {
-		inputs[0].classList.remove("is-valid");
-		inputs[0].classList.add("is-invalid");
-		inputs[0].nextElementSibling.classList.remove("valid-tooltip");
-		inputs[0].nextElementSibling.classList.add("invalid-tooltip");
-		inputs[0].nextElementSibling.innerHTML = "A password deve conter no mínimo 8 caracteres, e 1 número.";
-		return false;
-	}
-
 	if (inputs[0].value.trim() != inputs[1].value.trim()) {
 		inputs[1].classList.remove("is-valid");
 		inputs[1].classList.add("is-invalid");
