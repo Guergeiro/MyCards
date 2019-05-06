13909fdb5f34b3035535fea28d6d75675167d93a
EdwinOliveira <39379560+EdwinOliveira@users.noreply.github.com>
2019-05-06 09:50:16 +0100

    Update Premium.php
    Update da página premium, criação de cards de elementos

---
+++ application/views/pages/premium.php
@@ -0,0 +1,39 @@
+<div class="container-fluid">
+
+    <div class="row mt-5 text-center">
+        <div class="col-12">
+            <h1>Planos Premium do MyCards</h1>
+            <h5 class="mt-4">Nesta página estão apresentados os <strong>Plano Premium do MyCards</strong>.</h5>
+            <h6 class="mt-3">Estes planos consistem em desloquear funcionalidades para a sua Empresa no Mycards.</h6>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12">
+            <div class="row text-center">
+                <div class="col-12 col-sm-4 mt-2">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                        
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 mt-2">
+                    <div class="card">
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 mt-2">
+                    <div class="card">
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
