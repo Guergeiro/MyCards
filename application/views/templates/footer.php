51d5f09a7bb23f1957f311b7d9032f1e7bf33544
Breno Salles <breno-salles@hotmail.com>
2019-05-16 20:32:17 +0100

    Started footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,21 @@
+<footer>
+	<div class="bg-grey text-light">
+		<div class="container p-5">
+			<div class="col-md-3">
+				<h6 class="h6">Empresa</h6>
+				<a href="<?php echo base_url("faq"); ?>">FAQ</a>
+			</div>
+		</div>
+	</div>
+	<div class="bg-dark text-light">	
+		<div class="container p-3">
+			<div class="text-center mx-auto">
+				&copy; 2019 MyCards - <a href="https://choosealicense.com/licenses/gpl-3.0/" target="_blank" rel="noopener noreferrer">GNU GPLv3</a>
+			</div>
+		</div>
+	</div>
+</footer>
+
 <!-- Boostrap JavaScript -->
 <!-- JQuery -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
