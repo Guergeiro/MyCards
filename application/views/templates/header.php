fca0a17b83adb72678ddd3651c3be7d0c2b5b47a
Breno Salles <breno-salles@hotmail.com>
2019-05-20 22:56:27 +0100

    Adicionada data-session ao elemento DOM head para acesso fácil

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 
-<head>
+<head data-session='<?php echo json_encode($this->session->userdata()); ?>'>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
