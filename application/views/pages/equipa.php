19835f250aacf6df15d251a2b5252b9c4c0d7882
diogofalken <diogofalken0@gmail.com>
2019-05-30 12:46:14 +0100

    Ended Equipa

---
+++ application/views/pages/equipa.php
@@ -0,0 +1,68 @@
+<main class="container d-flex align-items-center justify-content-center py-4">
+  <div class="row">
+    <div class="col-lg-3 col-md-6 my-3">
+      <div class="card text-center">
+        <img src="<?php echo base_url("assets/imgs/team/breno.jpg")?>" alt="Breno Salles">
+        <div class="container">
+          <h2>Breno S.</h2>
+          <p class="title">Software/Web Developer</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>breno-salles@hotmail.com</p>
+          <p><a href="https://github.com/guergeiro/" target="_blank" role="button" class="btn btn-primary p-3"><i
+                class="fab fa-fw fa-2x fa-github" aria-hidden="true"></i></a><a
+              href="https://www.linkedin.com/in/sallesbreno/" target="_blank" role="button"
+              class="btn btn-primary p-3"><i class="fab fa-fw fa-2x fa-linkedin-in" aria-hidden="true"></i></a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 my-3">
+      <div class="card text-center">
+        <img src="<?php echo base_url("assets/imgs/team/falcao.jpg")?>" alt="Diogo Costa">
+        <div class="container">
+          <h2>Diogo C.</h2>
+          <p class="title">Software/Web Developer</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>diogofalken0@gmail.com</p>
+          <p><a href="https://github.com/diogofalken/" target="_blank" role="button" class="btn btn-primary p-3"><i
+                class="fab fa-fw fa-2x fa-github" aria-hidden="true"></i></a><a
+              href="https://www.linkedin.com/in/diogocosta99/" target="_blank" role="button"
+              class="btn btn-primary p-3"><i class="fab fa-fw fa-2x fa-linkedin-in" aria-hidden="true"></i></a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 my-3">
+      <div class="card text-center">
+        <img src="<?php echo base_url("assets/imgs/team/oliveira.jpg")?>" alt="Diogo Oliveira">
+        <div class="container">
+          <h2>Diogo O.</h2>
+          <p class="title">DataBase Admin</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>oliveiragamerptxxx@hi5.com</p>
+          <p><a href="https://github.com/EdwinOliveira/" target="_blank" role="button" class="btn btn-primary p-3"><i
+                class="fab fa-fw fa-2x fa-github" aria-hidden="true"></i></a><a
+              href="https://www.linkedin.com/in/diogo-oliveira-881047181/" target="_blank" role="button"
+              class="btn btn-primary p-3"><i class="fab fa-fw fa-2x fa-linkedin-in" aria-hidden="true"></i></a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 my-3">
+      <div class="card text-center">
+        <img src="<?php echo base_url("assets/imgs/team/filipe.jpg")?>" alt="Filipe Ribeiro">
+        <div class="container">
+          <h2>Filipe R.</h2>
+          <p class="title">Mobile Developer</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>filiperibeiroslb97@gmail.com</p>
+          <p><a href="https://github.com/filiperibeiro1904/" target="_blank" role="button"
+              class="btn btn-primary p-3"><i class="fab fa-fw fa-2x fa-github" aria-hidden="true"></i></a><a href="#"
+              target="_blank" role="button" class="btn btn-primary p-3"><i class="fab fa-fw fa-2x fa-linkedin-in"
+                aria-hidden="true"></i></a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
