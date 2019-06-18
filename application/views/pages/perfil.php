a7beedf45855e1c334f82abfc19373a0f40213d1
diogofalken <diogofalken0@gmail.com>
2019-06-18 16:28:05 +0100

    Done with Perfil bug

--- application/views/pages/perfil.php
+++ application/views/pages/perfil.php
@@ -1,45 +1,79 @@
 <main class="container my-3 py-3 d-flex align-items-center justify-content-center">
-	<div class="card w-100">
-		<div class="card-header">
-			<h3 class="text-primary text-truncate">Perfil</h3>
-			<small class="ml-1">Escolha o perfil de utilizador.</small>
-		</div>
-		<div class="card-body">
-			<div class="row">
-			<?php if ($this->session->flashdata("incorrectFlashData")): ?>
-			<div class="col-12 col-md-8 offset-md-2 text-center">
-				<div class="alert alert-danger"> <?= $this->session->flashdata("incorrectFlashData") ?> </div>
-			</div>
-			<?php endif; ?>
-			</div>
-		</div>
-	</div>
+  <div class="card w-100">
+    <div class="card-header">
+      <h3 class="text-primary text-truncate">Perfil</h3>
+      <small class="ml-1">Escolha o perfil de utilizador.</small>
+    </div>
+    <div class="card-body">
+      <div class="row">
+        <?php if ($this->session->flashdata("incorrectFlashData")): ?>
+        <div class="col-12 col-md-8 offset-md-2 text-center">
+          <div class="alert alert-danger"> <?= $this->session->flashdata("incorrectFlashData") ?> </div>
+        </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
 </main>
 
 <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-centered" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="modalTitle"></h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<i class="fas fa-times"></i>
-				</button>
-			</div>
-			<?php echo form_open("authentication/colaborador","onsubmit=\"return validation(this);\"") ?>
-			<div class="modal-body">
-				<div class="md-form">
-					<input type="hidden" id="nome" name="nome" class="form-control" readonly>
-				</div>
-				<div class="md-form">
-					<input type="password" name="codigoAcesso" id="codigoAcesso" class="form-control">
-					<label for="codigoAcesso">Código de Acesso</label>
-				</div>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-outline-primary" data-dismiss="modal">Fechar</button>
-				<button type="submit" class="btn btn-primary">Entrar</button>
-			</div>
-			</form>
-		</div>
-	</div>
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTitle"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <i class="fas fa-times"></i>
+        </button>
+      </div>
+      <?php echo form_open("authentication/colaborador","onsubmit=\"return validation(this);\"") ?>
+      <div class="modal-body">
+        <div class="md-form">
+          <input type="hidden" id="nome" name="nome" class="form-control" readonly>
+        </div>
+        <div class="md-form">
+          <input type="password" name="codigoAcesso" id="codigoAcesso" class="form-control">
+          <label for="codigoAcesso">Código de Acesso</label>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Fechar</button>
+        <button type="submit" class="btn btn-primary">Entrar</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="modalDono" tabindex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTitle">Novo colaborador</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <i class="fas fa-times"></i>
+        </button>
+      </div>
+      <form>
+        <div class="modal-body">
+          <div class="md-form">
+            <input type="text" name="nomeDono" id="nomeDono" class="form-control">
+            <label for="nomeDono">Nome</label>
+          </div>
+          <div class="md-form">
+            <input type="password" name="codigoAcessoDono" id="codigoAcessoDono" class="form-control">
+            <label for="codigoAcessoDono">Código de Acesso</label>
+          </div>
+          <div class="md-form">
+            <select id="colaboradorOption" name="dono" class="custom-select" disabled>
+              <option value="1">Dono</option>
+            </select>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Fechar</button>
+          <button type="button" class="btn btn-primary" id="criarDono">Criar</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -1,36 +1,78 @@
-$(document).ready(function () {
-	$.get("./api/empresa/" + JSON.parse(document.querySelector("head").getAttribute("data-session"))["ID_Empresa"] + "/colaborador", function (data) {
-		data = JSON.parse(data);
-		data.forEach(dataElement => {
-			let elementParent = document.querySelector(".row");
-			let div = document.createElement("div");
-			div.classList.add("col-sm-6", "col-md-3", "p-3");
+$(document).ready(function() {
+  $.get(
+    "./api/empresa/" +
+      JSON.parse(document.querySelector("head").getAttribute("data-session"))[
+        "ID_Empresa"
+      ] +
+      "/colaborador",
+    function(data) {
+      data = JSON.parse(data);
+      if (data.length == 0) {
+        document.querySelector(".row").innerHTML +=
+          '<div class="text-center col-12"><button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalDono"><i class="fas fa-plus fa-fw"></i></button></div>';
+      }
+      data.forEach(dataElement => {
+        let elementParent = document.querySelector(".row");
+        let div = document.createElement("div");
+        div.classList.add("col-sm-6", "col-md-3", "p-3");
 
-			let card = document.createElement("div");
-			card.classList.add("card", "h-100");
-			card.innerHTML = "<img class=\"card-img-top\" src=\"assets/avatar/" + dataElement.URL + ".png\">";
+        let card = document.createElement("div");
+        card.classList.add("card", "h-100");
+        card.innerHTML =
+          '<img class="card-img-top" src="assets/avatar/' +
+          dataElement.URL +
+          '.png">';
 
-			let cardBody = document.createElement("div");
-			cardBody.classList.add("card-body", "text-center");
-			cardBody.innerHTML = "<h4 class=\"card-title\">" + dataElement.Nome + "</h4><p class=\"card-text\">" + (dataElement.Dono == 1 ? "Dono" : "Colaborador") + " </p></div></div>";
-			card.appendChild(cardBody);
-			div.appendChild(card);
-			card.addEventListener("click", function () {
-				$("#modal").modal("toggle");
-				document.querySelector("#modalTitle").innerHTML = dataElement.Nome;
-				document.querySelector("#nome").value = dataElement.Nome;
-			});
-			elementParent.appendChild(div);
-		});
-	});
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body", "text-center");
+        cardBody.innerHTML =
+          '<h4 class="card-title">' +
+          dataElement.Nome +
+          '</h4><p class="card-text">' +
+          (dataElement.Dono == 1 ? "Dono" : "Colaborador") +
+          " </p></div></div>";
+        card.appendChild(cardBody);
+        div.appendChild(card);
+        card.addEventListener("click", function() {
+          $("#modal").modal("toggle");
+          document.querySelector("#modalTitle").innerHTML = dataElement.Nome;
+          document.querySelector("#nome").value = dataElement.Nome;
+        });
+        elementParent.appendChild(div);
+      });
+    }
+  );
+  document.querySelector("#criarDono").addEventListener("click", function() {
+    let okay = true;
+    document.querySelectorAll("#modalDono input").forEach(input => {
+      if (input.value.length == 0) {
+        okay = false;
+      }
+    });
+    if (okay) {
+      $.post(
+        "./api/empresa/" +
+          JSON.parse(
+            document.querySelector("head").getAttribute("data-session")
+          )["ID_Empresa"] +
+          "/colaborador",
+        {
+          nome: document.querySelector("#nomeDono").value,
+          codigoAcesso: document.querySelector("#codigoAcessoDono").value,
+          dono: document.querySelector("#colaboradorOption").value
+        },
+        location.reload(true)
+      );
+    }
+  });
 });
 
 function validation(form) {
-	let okay = true;
-	document.querySelectorAll("input").forEach(input => {
-		if (input.value.length == 0) {
-			okay = false;
-		}
-	});
-	return okay;
+  let okay = true;
+  form.querySelectorAll("input").forEach(input => {
+    if (input.value.length == 0) {
+      okay = false;
+    }
+  });
+  return okay;
 }
