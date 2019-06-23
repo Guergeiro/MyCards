735a71513846a7820bfbe4194bede3f2aab41561
diogofalken <diogofalken0@gmail.com>
2019-06-23 12:03:53 +0100

    Ended "Visualizar Empresa" page

---
+++ application/views/pages/visualizarEmpresa.php
@@ -0,0 +1,82 @@
+<main class="container my-3 py-3">
+  <div class="card">
+    <div class="card-header">
+      <h3 class="text-primary">Visualizar Empresa</h3>
+      <small class="ml-1">Aqui consegue visualizar todos os dados da sua empresa.</small>
+    </div>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-signature prefix"></i>
+            <input type="text" id="nome" name="nome" class="form-control">
+            <label for="nome">Nome</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-envelope prefix"></i>
+            <input type="text" id="email" name="email" class="form-control">
+            <label for="email">Email</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-lock prefix"></i>
+            <input type="password" id="password" name="password" class="form-control" value="manaemjeff" disabled>
+            <label for="password">Password</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-location-arrow prefix"></i>
+            <input type="text" id="localizacao" name="localizacao" class="form-control">
+            <label for="localizacao">Localizacao</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-id-badge prefix"></i>
+            <input type="text" id="nif" name="nif" class="form-control">
+            <label for="nif">NIF</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-user-friends prefix"></i>
+            <input type="text" id="colaboradores" name="colaboradores" class="form-control">
+            <label for="colaboradores">Colaboradores</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-credit-card prefix"></i>
+            <input type="text" id="cartoes" name="cartoes" class="form-control">
+            <label for="cartoes">Clientes</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="md-form mb-0">
+            <i class="fas fa-audio-description prefix"></i>
+            <input type="text" id="plano" name="plano" class="form-control">
+            <label for="plano">Plano de Subscrição</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
