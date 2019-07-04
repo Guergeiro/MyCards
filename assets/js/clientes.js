ba26a834fa11c73d0c9ae941ccea4a50632dab1b
EdwinOliveira <39379560+EdwinOliveira@users.noreply.github.com>
2019-07-04 11:26:00 +0100

    Update clientes.js

--- assets/js/clientes.js
+++ assets/js/clientes.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
                     data.forEach(da => {
                         document.querySelector("table tbody").innerHTML +=
                             '<tr data-url="' +
-                            cartao["ID_Cartao"] +
+                            cartoes["ID_Cartao"] +
                             '"><th scope="row">' +
                             da["ID_Cliente"] +
                             "</th><td>" +
