d66b3153fa3c396772b68d2be0f6739d4d4e8bf0
Guergeiro <breno-salles@hotmail.com>
2019-07-05 13:32:11 +0100

    Added error messages

--- assets/js/admin_admin.js
+++ assets/js/admin_admin.js
@@ -143,9 +143,7 @@ const key = "b3c7dc7d8f72f52ae14527a8da25979e";
 
 const getInfoEmpresa = async (nif) => {
     const response = await fetch(`https://www.nif.pt/?json=1&q=${nif}&key=${key}`, {
-        headers: {
-            "Content-Type": "application/json"
-        }
+        credentials: "include"
     });
     const data = await response.json();
     return data;
@@ -165,15 +163,18 @@ $(document).ready(function () {
         // Update the modal"s content. We"ll use jQuery here, but you could use a data binding library or other methods instead.
         const modal = document.querySelector("#modal");
         modal.querySelector("#modalTitle").innerHTML = recipient;
+        console.log(modal.querySelector(".modal-body .container-fluid .row"))
         getEmpresaById(recipient).then(empresa => {
             getInfoEmpresa(empresa["NIF"]).then(data => {
                 if (data["result"] == "error") {
                     // Não existe nif ou nif é pessoal
-                    modal.querySelector(".modal-body container-fluid row").innerHTML = `<div class="col-12">O NIF indicado é válido mas não conseguimos determinar a entidade associada.</div>`;
+                    modal.querySelector(".modal-body .container-fluid .row").innerHTML = `<div class="col-12 alert alert-warning text-center" role="alert">O NIF indicado é válido mas não conseguimos determinar a entidade associada.</div>`;
                 } else {
                     // Tudo ok
-                    modal.querySelectorAll(".modal-body container-fluid row").innerHTML = `<div class="col-md-12>Nome: ${data["records"][nif]["title"]}</div><div class="col-md-6">NIF: ${nif}</div><div class="col-md-6">Cidade: ${data["records"][nif]["city"]}</div><div class="col-md-6">Email: ${data["records"][nif]["contacts"]["email"]}</div><div class="col-md-6">Telefone: ${data["records"][nif]["contacts"]["phone"]}</div>`;
+                    modal.querySelectorAll(".modal-body .container-fluid .row").innerHTML = `<div class="col-md-12>Nome: ${data["records"][nif]["title"]}</div><div class="col-md-6">NIF: ${nif}</div><div class="col-md-6">Cidade: ${data["records"][nif]["city"]}</div><div class="col-md-6">Email: ${data["records"][nif]["contacts"]["email"]}</div><div class="col-md-6">Telefone: ${data["records"][nif]["contacts"]["phone"]}</div>`;
                 }
+            }).catch(err => {
+                modal.querySelector(".modal-body .container-fluid .row").innerHTML = `<div class="col-12 alert alert-danger text-center" role="alert">Ocorreu um erro a ir buscar a informação. Tente mais tarde.</div>`;
             });
         });
     });
